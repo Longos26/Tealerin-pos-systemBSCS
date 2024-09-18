d657f9b476ef676042645caac97d1c619478fcf1
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2024-09-17 23:25:42 -0500

    55

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+//this a navbar and displaying a Homepage.js dito yung mga outpu
+//connected ito sa DefaultLayout.css
+import React, { useState,useEffect} from "react";
+import {useSelector} from "react-redux";
 import { Layout, Menu } from "antd";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
@@ -15,75 +17,76 @@ import {
 import "../styles/DefaultLayout.css";
 const { Header, Sider, Content } = Layout;
 
-const DefaultLayout = ({ children }) => {
-  const { cartItems } = useSelector((state) => state.rootReducer);
-  const [collapsed, setCollapsed] = useState(false);
+const DefaultLayout =({children}) => {
+  const {cartItems} = useSelector(state => state.rootReducer);
+  const [collapsed,setCollapsed] = useState(false)
+  const navigate = useNavigate();
 
   const toggle = () => {
-    setCollapsed(!collapsed);
+    setCollapsed(
+      !collapsed);
   };
-  //to get localstorage data
-  useEffect(() => {
-    localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  }, [cartItems]);
+  //to get local storage
+useEffect(() => {
+ localStorage.getItem("cartItems", JSON.stringify(cartItems));
+}, [cartItems]);
 
-  return (
-    <Layout>
-      <Sider trigger={null} collapsible collapsed={collapsed}>
-            <div className="logo">
+    return (
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="logo">
   <img src="./assets/Untitled124.png" alt="Logo" className="logo-image" />
 </div>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={window.location.pathname}
+          >
+            <Menu.Item key="/" icon={<HomeOutlined />}>
+              <Link to="/">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="/bills" icon={<CopyOutlined />}>
+              <Link to="/bills">Bills</Link>
+            </Menu.Item>
+            <Menu.Item key="/items" icon={<UnorderedListOutlined />}>
+              <Link to="/items">Items</Link>
+            </Menu.Item>
+            <Menu.Item key="/customers" icon={<UserOutlined />}>
+              <Link to="/customers">Cutomers</Link>
+            </Menu.Item>
+            <Menu.Item key="/logout" icon={<LogoutOutlined />}>
+              Logout
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(
+              collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: "trigger",
+                onClick: toggle,
+              }
+            )}
 
-
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={window.location.pathname}
-        >
-          <Menu.Item key="/" icon={<HomeOutlined />}>
-            <Link to="/">Home</Link>
-          </Menu.Item>
-          <Menu.Item key="/bills" icon={<CopyOutlined />}>
-            <Link to="/bills">Bills</Link>
-          </Menu.Item>
-          <Menu.Item key="/items" icon={<UnorderedListOutlined />}>
-            <Link to="/items">Items</Link>
-          </Menu.Item>
-          <Menu.Item key="/customers" icon={<UserOutlined />}>
-            <Link to="/customers">Cutomers</Link>
-          </Menu.Item>
-          <Menu.Item key="/logout" icon={<LogoutOutlined />}>
-            Logout
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }}>
-          {React.createElement(
-            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-            {
-              className: "trigger",
-              onClick: toggle,
-            }
-          )}
-          <div className="cart-item d-flex jusitfy-content-space-between flex-row" on>
-            <p>{cartItems.length}</p>
-            <ShoppingCartOutlined />
-          </div>
-        </Header>
-        <Content
-          className="site-layout-background"
-          style={{
-            margin: "24px 16px",
-            padding: 24,
-            minHeight: 280,
-          }}
-        >
-          {children}
-        </Content>
+            <div className="cart-items d-flex align-items-center" onClick={() => navigate("/cart")}>
+              <p>{cartItems.length}</p>
+              <ShoppingCartOutlined />
+            </div>
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            {children}
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
-  );
-};
+    );
+}
 
 export default DefaultLayout;
