0b295b3c1d168ac67a58c8d52a3297d2bcbd4b71
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2024-10-05 02:07:35 -0500

    df

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,29 +1,30 @@
 const itemModel = require("../models/itemModel");
 
+// get categories
 const getItemController = async (req, res) => {
   try {
     const items = await itemModel.find();
     res.status(200).send(items);
   } catch (error) {
     console.log(error);
-    res.status(500).send({ message: "Internal Server Error" });
   }
 };
 
-// Add categories
+
+
+//add categories
 const addItemController = async (req, res) => {
   try {
     const newItem = new itemModel(req.body);
     await newItem.save();
-    res.status(201).send({ message: "Item Created Successfully!" });
+    res.status(201).send("Item Created Successfully!");
   } catch (error) {
+    res.status(400).send("error", error);
     console.log(error);
-    // Send a response with a message and the error details
-    res.status(400).send({ message: "Error creating item", error: error.message });
   }
 };
 
-// Update item
+//update item
 const editItemController = async (req, res) => {
   try {
     const { itemId } = req.body;
@@ -31,23 +32,23 @@ const editItemController = async (req, res) => {
     await itemModel.findOneAndUpdate({ _id: itemId }, req.body, {
       new: true,
     });
-    res.status(200).json({ message: "Item Updated" });
+
+    res.status(201).json("item Updated");
   } catch (error) {
+    res.status(400).send(error);
     console.log(error);
-    res.status(400).send({ message: "Error updating item", error: error.message });
   }
 };
-
-// Delete item
+//delete item
 const deleteItemController = async (req, res) => {
   try {
     const { itemId } = req.body;
     console.log(itemId);
     await itemModel.findOneAndDelete({ _id: itemId });
-    res.status(200).json({ message: "Item Deleted" });
+    res.status(200).json("item Deleted");
   } catch (error) {
+    res.status(400).send(error);
     console.log(error);
-    res.status(400).send({ message: "Error deleting item", error: error.message });
   }
 };
 
--- seeder.js
+++ seeder.js
@@ -21,7 +21,6 @@ const importData = async () => {
   }
 
   
-  
 };
 
 importData();
