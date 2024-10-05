83f795f618c57043055e7ffa301c4805751bc6d7
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2024-10-05 01:56:55 -0500

    hj

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,30 +1,29 @@
 const itemModel = require("../models/itemModel");
 
-// get categories
 const getItemController = async (req, res) => {
   try {
     const items = await itemModel.find();
     res.status(200).send(items);
   } catch (error) {
     console.log(error);
+    res.status(500).send({ message: "Internal Server Error" });
   }
 };
 
-
-
-//add categories
+// Add categories
 const addItemController = async (req, res) => {
   try {
     const newItem = new itemModel(req.body);
     await newItem.save();
-    res.status(201).send("Item Created Successfully!");
+    res.status(201).send({ message: "Item Created Successfully!" });
   } catch (error) {
-    res.status(400).send("error", error);
     console.log(error);
+    // Send a response with a message and the error details
+    res.status(400).send({ message: "Error creating item", error: error.message });
   }
 };
 
-//update item
+// Update item
 const editItemController = async (req, res) => {
   try {
     const { itemId } = req.body;
@@ -32,23 +31,23 @@ const editItemController = async (req, res) => {
     await itemModel.findOneAndUpdate({ _id: itemId }, req.body, {
       new: true,
     });
-
-    res.status(201).json("item Updated");
+    res.status(200).json({ message: "Item Updated" });
   } catch (error) {
-    res.status(400).send(error);
     console.log(error);
+    res.status(400).send({ message: "Error updating item", error: error.message });
   }
 };
-//delete item
+
+// Delete item
 const deleteItemController = async (req, res) => {
   try {
     const { itemId } = req.body;
     console.log(itemId);
     await itemModel.findOneAndDelete({ _id: itemId });
-    res.status(200).json("item Deleted");
+    res.status(200).json({ message: "Item Deleted" });
   } catch (error) {
-    res.status(400).send(error);
     console.log(error);
+    res.status(400).send({ message: "Error deleting item", error: error.message });
   }
 };
 
